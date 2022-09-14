123594d8ca1f1c23a415eaa0cd8b51e6de10f6d9
SleepyyPlayz <16306416+SleepyyPlayz@users.noreply.github.com>
2022-09-13 23:06:39 -0400

    Completed All TODOs

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -3,24 +3,24 @@ import java.util.Arrays;
 /**
  * This file contains a few exercises to familiarize you with specific
  * class features in Java.
- *
+ * .
  * You should read this file from top-to-bottom. Any tasks you are to complete
- * are labelled with TODO
- *
+ * are labelled with TO-DO
+ * .
  * For your convenience, we have also included references to the
  * relevant readings for each task.
  */
 
 public class Silly implements Comparable<Silly>{
-    /**
+    /*
      * 1. Java has variables known as "static" variables.
      * These are variables that exist in every instance of a class,
      * and which have the same value across all instances.
-     *
+     * .
      * Below we have created a static variable (signified by the keyword
      * static) named my_static. The countStatic() method makes use of this
      * static variable.
-     *
+     * .
      * (Relevant reading: 2.1. Static variables)
      */
     public static int my_static = 0;
@@ -41,14 +41,14 @@ public class Silly implements Comparable<Silly>{
         return to_return;
     }
 
-    /**
+    /*
      * 2. Java allows for us to overload methods: this means we can have
      *    multiple methods of the same name, but which take different
      *    parameters (both in terms of numbers and type).
-     *
+     *    .
      *    Below we have created two different constructors and have left
      *    a third for you to write.
-     *
+     *    .
      *    (Relevant reading: 2.2. Overloading)
      */
 
@@ -72,20 +72,27 @@ public class Silly implements Comparable<Silly>{
         this.name = "Silly #" + number;
     }
 
-    /**
-     * TODO (Task 1): Create another constructor that takes in TWO parameters,
+    /*
+     * TO-DO (Task 1): Create another constructor that takes in TWO parameters,
      *       both of which are strings.
      *       Afterwards, set this.name to the concatenation of both
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
 
-
-
-
+    /**
+     * Creates a new Silly object.
+     * This constructor takes in two strings as arguments.
+     *
+     * @param first first string parameter for constructing the name
+     * @param second second string parameter for constructing the name
+     */
+    public Silly(String first, String second){
+        this.name = first + second;
+    }
 
     public static void main(String[] args) {
-        /**
+        /*
          * Task 1 (continued): The below demonstrates how each of the Silly
          *                     constructors are called.
          *
@@ -106,8 +113,8 @@ public class Silly implements Comparable<Silly>{
         Silly x = new Silly("something");
         Silly y = new Silly("something else");
 
-        /**
-         * TODO (Task 2): Below are a bunch of calls to countStatic using
+        /*
+         * TO-DO (Task 2): Below are a bunch of calls to countStatic using
          *                two different instances of Silly.
          *                Fill out the expected_values array with the
          *                values that countStatic will return at each call.
@@ -116,7 +123,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -124,23 +131,23 @@ public class Silly implements Comparable<Silly>{
     /**
      * 3. Whenever we try to build a string with an Object, the toString()
      *    method is called. This is equivalent to the __str__ method in Python.
-     *
+     * .
      *    (Relevant reading: 2.3. toString)
-     *
+     * .
      * Return the String representation of this Silly object (i.e. its name).
      *
      * @return the name of this Silly.
      */
     @Override
     public String toString(){
-        // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
      * 4. We can also override the equals() method, which is the equivalent of
      *    Python's __eq__; however, unlike Python, this is called when we use
      *    .equals() rather than == (which checks for ID equality in Java).
-     *
+     *    .
      *    (Relevant reading: 2.4. Equality)
      *
      * @param o   the object to compare to
@@ -148,8 +155,8 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public boolean equals(Object o) {
-        /**
-         * TODO (Task 4): Complete the body of this method!
+        /*
+         * TO-DO (Task 4): Complete the body of this method!
          *                We've started it by checking the type of o for you.
          *                You just need to return true if the names are equal.
          */
@@ -161,6 +168,7 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        return other.name.equals(this.name);
     }
 
     /**
@@ -171,11 +179,11 @@ public class Silly implements Comparable<Silly>{
      *    ii) override the compareTo method. This takes another object
      *        (of the same type if we specify the classname when we write
      *        'implements Comparable'), and returns an integer.
-     *
+     * .
      *    (Relevant reading: 2.5. Comparing objects)
-     *
+     * .
      * Compares this object with other for order.
-     *
+     * .
      * Returns a negative integer, zero, or a positive integer as this
      * object is less than, equal to, or greater than the specified object.
      *
@@ -185,8 +193,8 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public int compareTo(Silly other) {
-        /**
-         * TODO (Task 5): Implement the body of this method.
+        /*
+         * TO-DO (Task 5): Implement the body of this method.
          *                A positive number should be returned if this.name
          *                is longer than other.name, a negative number if
          *                other.name is longer than this.name, and 0 if
@@ -194,10 +202,18 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+
+        if(this.name.length() > other.name.length()){
+            return 1;
+        }else if(this.name.length() < other.name.length()){
+            return -1;
+        }else{
+            return 0;
+        }
     }
 
     /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
+     * TO-DO (Task 6): Submit the changes you made on GitHub!
      *                When you submit it, go to the 'Actions' tab. You should
      *                be able to see this exercise being autograded!
      *
